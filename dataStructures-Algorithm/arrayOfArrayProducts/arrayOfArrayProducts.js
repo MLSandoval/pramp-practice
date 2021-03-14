18b960c659a5b1c41734cb5bbfacb769fdd29106
mlsandoval <michaelsandoval8@gmail.com>
2021-03-13 21:22:53 -0800

    added array of array products question, tricky array manipulation

---
+++ dataStructures-Algorithm/arrayOfArrayProducts/arrayOfArrayProducts.js
@@ -0,0 +1,78 @@
+function arrayOfArrayProducts(arr) {
+  // your code goes here
+  
+  //forEach 
+  //
+  
+  let i = 0;
+  let j = 0;
+  
+  let total = 1;
+  
+  for(let i = 0 < arr.length - 1; i++){
+    
+    
+    let current = arr[i]; // 8
+    
+    total = 
+    
+  }
+  
+   for(let j = 0; < arr.length; i++){
+    
+    let current = arr[i];
+     
+    if(){
+       
+    }
+  }
+  
+}
+
+/*
+
+
+  function calcProductArray(arr):
+    n = arr.length
+    if(n == 0 OR n == 1):
+        # no values to multiply if n equals to 0 or 1
+        return []
+
+    productArr = []
+    product = 1
+    for i from 0 to n-1:
+        productArr[i] = product
+        product *= arr[i]
+
+    product = 1
+    for i from n-1 to 0:
+        productArr[i] *= product
+        product *= arr[i]
+
+    return productArr
+    
+    
+
+
+  let leftProduct = 1;
+  let product_left = [1]
+  for ...1-> length:
+    leftProduct *= arr[i]
+    
+  product_entry = product_left * product_right
+  arr = [8, 10, 2 , 4 , 5 , 6]
+             ^
+  leftProduct= [1, 8, 80]
+  rightProduct = [20 ,2 , 1]
+
+  i:arr
+  o: arr
+  
+  0 <= length <= 20
+  
+  - traverse through the array
+  - for each index we encounter, solve the product
+    of all other values in the array, except val at current index
+  - return arr with our values
+  
+*/
